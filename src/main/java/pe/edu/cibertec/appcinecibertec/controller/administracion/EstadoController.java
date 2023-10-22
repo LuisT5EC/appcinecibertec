51a6e36e0ad3630e4df82fc3b1ae91922538475d
sandy11JsDs <jorgesandy965@gmail.com>
2023-10-22 12:18:22 -0500

    Lista - ESTADOS

---
+++ src/main/java/pe/edu/cibertec/appcinecibertec/controller/administracion/EstadoController.java
@@ -0,0 +1,23 @@
+package pe.edu.cibertec.appcinecibertec.controller.administracion;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.cibertec.appcinecibertec.service.EstadoService;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/administracion/estado")
+public class EstadoController {
+
+    private EstadoService estadoService;
+    @GetMapping("/frmestado")
+    public String index(Model model){
+        model.addAttribute("listarestados",
+                estadoService.listarEstados());
+        return "administracion/frmestado";
+    }
+}
