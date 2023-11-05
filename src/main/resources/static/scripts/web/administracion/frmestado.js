ef4b40ff63976e0bfa93f0d5b5a37dab4d7e3bdb
lsalvatierra89 <l.salvat@gmail.com>
2023-11-05 09:46:20 -0500

    manejo de botones nuevo y actualizar

--- src/main/resources/static/scripts/web/administracion/frmestado.js
+++ src/main/resources/static/scripts/web/administracion/frmestado.js
@@ -1,4 +1,17 @@
 $(document).on("click", "#btnnuevo", function(){
     //alert("Hola estoy con JQUERY");
+    $("#txtdescripcion").val("");
+    $("#hddidestado").val("0")
+    $("#modalestado").modal("show");
+});
+
+$(document).on("click", ".btnactualizar", function(){
+    $("#modalestado").modal("show");
+    $("#txtdescripcion").val($(this).attr("data-descestado"));
+    $("#hddidestado").val($(this).attr("data-idestado"));
+});
+
+$(document).on("click", ".btneliminar", function(){
+    //alert("Hola estoy con JQUERY");
     $("#modalestado").modal("show");
 });
