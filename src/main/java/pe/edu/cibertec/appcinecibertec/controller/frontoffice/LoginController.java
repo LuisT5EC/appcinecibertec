59df1d815e59356176bee9c8d817f6c2658a854f
lsalvatierra89 <l.salvat@gmail.com>
2023-11-19 11:37:15 -0500

    formularios spring security

---
+++ src/main/java/pe/edu/cibertec/appcinecibertec/controller/frontoffice/LoginController.java
@@ -0,0 +1,30 @@
+package pe.edu.cibertec.appcinecibertec.controller.frontoffice;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.cibertec.appcinecibertec.model.bd.Usuario;
+import pe.edu.cibertec.appcinecibertec.service.UsuarioService;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/auth")
+public class LoginController {
+    private UsuarioService usuarioService;
+    @GetMapping("/login")
+    public String login(){
+        return "frontoffice/auth/login";
+    }
+    @GetMapping("/registrar")
+    public String registro(){
+        return "frontoffice/auth/registro";
+    }
+    @PostMapping("/guardarusuario")
+    public String guardarUsuario(@ModelAttribute Usuario usuario){
+        usuarioService.saveUser(usuario);
+        return "frontoffice/auth/login";
+    }
+}
---
+++ src/main/java/pe/edu/cibertec/appcinecibertec/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package pe.edu.cibertec.appcinecibertec.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.appcinecibertec.model.bd.Rol;
+import pe.edu.cibertec.appcinecibertec.model.bd.Usuario;
+import pe.edu.cibertec.appcinecibertec.repository.RolRepository;
+import pe.edu.cibertec.appcinecibertec.repository.UsuarioRepository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+@AllArgsConstructor
+public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+    private RolRepository rolRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder =
+            new BCryptPasswordEncoder();
+    public Usuario findByNomusuario(String nomusuario){
+        return usuarioRepository.findByNomusuario(nomusuario);
+    }
+    public Usuario saveUser(Usuario usuario){
+        usuario.setPassword(
+                bCryptPasswordEncoder.encode(usuario.getPassword())
+        );
+        Rol usuarioRol = rolRepository.findByNomrol("USER");
+        usuario.setRoles(new HashSet<>(Arrays.asList(usuarioRol)));
+        return usuarioRepository.save(usuario);
+    }
+
+
+}
